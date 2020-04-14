4e6dcfd160c2b1e414339b992f7003df7fbb4ede
ASUS <yasamanzolfi4@gmail.com>
2020-04-14 22:36:31 +0430

    Create Category And SubCategory Classes

--- src/main/java/model/category/Category.java
+++ src/main/java/model/category/Category.java
@@ -1,5 +1,37 @@
 package model.category;
 
+import java.util.ArrayList;
+
 public class Category {
+    private String name;
+    private ArrayList<String> details;
+    private ArrayList<SubCategory> subCategories;
+
+    public Category(String name, ArrayList<String> details, ArrayList<SubCategory> subCategories) {
+        this.name = name;
+        this.details = details;
+        this.subCategories = subCategories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ArrayList<String> getDetails() {
+        return details;
+    }
+
+    public ArrayList<SubCategory> getSubCategories() {
+        return subCategories;
+    }
+
+    public void addSubCategory(SubCategory subCategory) {
+        this.subCategories.add(subCategory);
+        subCategory.setParentCategory(this);
+    }
+
+    public void removeSubCategory(SubCategory subCategory) {
+        this.subCategories.remove(subCategory);
+    }
 
 }
--- src/main/java/model/category/SubCategory.java
+++ src/main/java/model/category/SubCategory.java
@@ -1,4 +1,46 @@
 package model.category;
 
+import model.productThings.Good;
+
+import java.util.ArrayList;
+
 public class SubCategory {
+    private String name;
+    private Category parentCategory;
+    private ArrayList<String> details;
+    private ArrayList<Good> goods;
+
+    public SubCategory(String name, ArrayList<String> details) {
+        this.name = name;
+        this.details = details;
+        this.goods = new ArrayList<>();
+    }
+
+    public void setParentCategory(Category parentCategory) {
+        this.parentCategory = parentCategory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category getParentCategory() {
+        return parentCategory;
+    }
+
+    public ArrayList<String> getDetails() {
+        return details;
+    }
+
+    public ArrayList<Good> getGoods() {
+        return goods;
+    }
+
+    public void addGood(Good good){
+        this.goods.add(good);
+    }
+
+    public void removeGood(Good good){
+        this.goods.remove(good);
+    }
 }
