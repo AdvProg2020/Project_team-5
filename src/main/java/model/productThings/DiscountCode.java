efb944d2c704c64727bfeea57176770ecf654b33
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-04-15 01:44:30 +0430

    Create DiscountCode class

--- src/main/java/model/productThings/DiscountCode.java
+++ src/main/java/model/productThings/DiscountCode.java
@@ -1,4 +1,75 @@
 package model.productThings;
 
+import model.persons.Customer;
+
+import java.util.Date;
+import java.util.HashMap;
+
 public class DiscountCode {
+    private String code;
+    private Date startDate;
+    private Date endDate;
+    private Long MaxDiscountAmount;
+    private int discountPercent;
+    private HashMap<Customer, Integer> includedCustomers;
+
+    public DiscountCode(String code, Date startDate, Date endDate, Long maxDiscountAmount, int discountPercent, HashMap<Customer, Integer> includedCustomers) {
+        this.code = code;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        MaxDiscountAmount = maxDiscountAmount;
+        this.discountPercent = discountPercent;
+        this.includedCustomers = includedCustomers;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public Long getMaxDiscountAmount() {
+        return MaxDiscountAmount;
+    }
+
+    public int getDiscountPercent() {
+        return discountPercent;
+    }
+
+    public HashMap<Customer, Integer> getIncludedCustomers() {
+        return includedCustomers;
+    }
+
+    public void setDiscountPercent(int discountPercent) {
+        this.discountPercent = discountPercent;
+    }
+
+    public void setMaxDiscountAmount(Long maxDiscountAmount) {
+        MaxDiscountAmount = maxDiscountAmount;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public static String generateRandomDiscountCode() {
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvxyz";
+        StringBuilder randomCode = new StringBuilder(15);
+
+        for (int i = 0; i < 15; i++) {
+            int index = (int)(AlphaNumericString.length() * Math.random());
+            randomCode.append(AlphaNumericString.charAt(index));
+        }
+        return randomCode.toString();
+    }
 }
