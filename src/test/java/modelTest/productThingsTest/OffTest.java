8ddf936a2331d662f731490c41377394ae897f61
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-04-16 01:24:39 +0430

    Edit test for Off class

---
+++ src/test/java/modelTest/productThingsTest/OffTest.java
@@ -0,0 +1,34 @@
+package modelTest.productThingsTest;
+
+import model.persons.Seller;
+import model.productThings.Good;
+import model.productThings.Off;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class OffTest {
+    private static Off off;
+    private static Good good;
+    private static ArrayList<Good> offGoods;
+
+    @Before
+    public void initializeNecessaryValuesForTest() {
+        Seller seller = new Seller("amoo", "sadegh", "majid", "sadegh@gmail.com", "09360000000", "1234");
+        good = new Good("sosise", "gooshtiran", null, "vanak sosise", null, seller, 50000L, 4);
+        offGoods = new ArrayList<>();
+        offGoods.add(good);
+        off = new Off(offGoods, LocalDate.parse("2020-05-06"), LocalDate.parse("2020-07-06"), 100000L, 25, seller);
+    }
+
+    @Test
+    public void PriceAfterOffTest() {
+        long expectedPrice = 37500L;
+        long actualValue = off.getPriceAfterOff(good);
+        Assert.assertEquals(expectedPrice, actualValue);
+    }
+}
