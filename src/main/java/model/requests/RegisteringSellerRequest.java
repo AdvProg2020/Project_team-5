ee9c114831db79e31e7591bb7fc6693cf726dbaa
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-04-15 15:45:30 +0430

    Add RegisteringSellerRequest and EdittingGoodRequest classes and edit off class

--- src/main/java/model/requests/RegisteringSellerRequest.java
+++ src/main/java/model/requests/RegisteringSellerRequest.java
@@ -1,4 +1,27 @@
 package model.requests;
 
+import model.Shop;
+import model.persons.Seller;
+
 public class RegisteringSellerRequest extends Request {
+    private Seller seller;
+
+    public RegisteringSellerRequest(Seller seller) {
+        this.seller = seller;
+    }
+
+    public Seller getSeller() {
+        return seller;
+    }
+
+    @Override
+    public String toString() {
+        return "RegisteringSellerRequest\n" +
+                "seller Username : " + seller.getUsername() + "\n";
+    }
+
+    @Override
+    public void acceptRequest() {
+        Shop.getInstance().addPerson(seller);
+    }
 }
