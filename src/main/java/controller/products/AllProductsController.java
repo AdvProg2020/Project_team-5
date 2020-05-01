18f05fa0d39bdf4aea1961d3f2ef3ff00df472a5
aboots <mahid.abootorabi2@gmail.com>
2020-05-02 03:37:58 +0430

    work on show a product menu

--- src/main/java/controller/products/AllProductsController.java
+++ src/main/java/controller/products/AllProductsController.java
@@ -1,5 +1,7 @@
 package controller.products;
 
+import controller.MainController;
+import exception.ProductNotFoundException;
 import model.Shop;
 import model.category.Category;
 
@@ -9,10 +11,18 @@ public class AllProductsController {
 
     public String showCategories() {
         ArrayList<Category> allCategories = Shop.getInstance().getAllCategories();
-        String output ="";
+        String output = "";
         for (Category category : allCategories) {
-            output +=category.toString();
+            output += category.toString();
         }
         return output;
+    }
+
+    public void showAProduct(long id) throws ProductNotFoundException {
+        if (Shop.getInstance().findGoodById(id) == null){
+            throw new ProductNotFoundException();
+        }else{
+            MainController.getInstance().getProductController().setGood(Shop.getInstance().findGoodById(id));
+        }
     }
 }
--- src/main/java/controller/products/ProductController.java
+++ src/main/java/controller/products/ProductController.java
@@ -1,4 +1,15 @@
 package controller.products;
 
+import model.productThings.Good;
+
 public class ProductController {
+    private Good good;
+
+    public Good getGood() {
+        return good;
+    }
+
+    public void setGood(Good good) {
+        this.good = good;
+    }
 }
