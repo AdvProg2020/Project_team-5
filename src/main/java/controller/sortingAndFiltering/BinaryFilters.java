d98ab1851cbe2ef2f294f163a5fbfd18698e5cdf
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-05-11 19:53:13 +0430

    create binary filter class

--- src/main/java/controller/sortingAndFiltering/BinaryFilters.java
+++ src/main/java/controller/sortingAndFiltering/BinaryFilters.java
@@ -1,4 +1,33 @@
 package controller.sortingAndFiltering;
 
 public class BinaryFilters  {
+    private String filterName;
+    private String startValue;
+    private String endValue;
+
+    public BinaryFilters(String filterName, String startValue, String endValue) {
+        this.filterName = filterName;
+        this.startValue = startValue;
+        this.endValue = endValue;
+    }
+
+    public String getFilterName() {
+        return filterName;
+    }
+
+    public String getStartValue() {
+        return startValue;
+    }
+
+    public String getEndValue() {
+        return endValue;
+    }
+
+    public void setStartValue(String startValue) {
+        this.startValue = startValue;
+    }
+
+    public void setEndValue(String endValue) {
+        this.endValue = endValue;
+    }
 }
