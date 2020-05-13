e57f664c7e24d07cb89fdd8ec1751563a0c06ac9
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-05-14 01:51:26 +0430

    delete files

--- src/main/java/model/database/DeletingData.java
+++ src/main/java/model/database/DeletingData.java
@@ -1,5 +1,6 @@
 package model.database;
 
+import exception.FileCantBeDeletedException;
 import model.category.Category;
 import model.category.SubCategory;
 import model.orders.OrderForCustomer;
@@ -9,21 +10,45 @@ import model.persons.Manager;
 import model.persons.Seller;
 import model.productThings.*;
 
+import java.io.File;
+
 public class DeletingData {
-    public void deleteManager(Manager manager) {
-
+    public void deleteManager(Manager manager) throws FileCantBeDeletedException {
+        String filePath = "Resources\\Users\\Managers\\" + manager.getUsername() + ".json";
+        File file = new File(filePath);
+        if (!file.delete())
+            throw new FileCantBeDeletedException();
     }
 
-    public void deleteCustomer(Customer customer) {
-
+    public void deleteCustomer(Customer customer) throws FileCantBeDeletedException {
+        String filePath = "Resources\\Users\\Customers\\" + customer.getUsername() + ".json";
+        File file = new File(filePath);
+        if (!file.delete())
+            throw new FileCantBeDeletedException();
     }
 
-    public void deleteSeller(Seller seller) {
-
+    public void deleteSeller(Seller seller) throws FileCantBeDeletedException {
+        String filePath = "Resources\\Users\\Sellers\\" + seller.getUsername() + ".json";
+        File file = new File(filePath);
+        if (!file.delete())
+            throw new FileCantBeDeletedException();
     }
 
-    public void deleteProduct(Good good) {
-
+    public void deleteProduct(Good good) throws FileCantBeDeletedException {
+        String filePath = "Resources\\Products\\product" + good.getGoodId() + ".json";
+        File file = new File(filePath);
+        if (!file.delete())
+            throw new FileCantBeDeletedException();
+        for (SellerRelatedInfoAboutGood infoAboutGood : good.getSellerRelatedInfoAboutGoods()) {
+            filePath = "Resources\\ProductsInfo\\productInfo" + good.getGoodId() + "_" + infoAboutGood.getSeller().getUsername() + ".json";
+            file = new File(filePath);
+            if (!file.delete())
+                throw new FileCantBeDeletedException();
+        }
+        for (Comment comment : good.getComments()) {
+            deleteComment(comment);
+        }
+        //remove rates
     }
 
     public void deleteDiscount(DiscountCode discountCode) {
