a3899ff82e3ef666d81223a95c15ea636881b554
ASUS <yasamanzolfi4@gmail.com>
2020-05-13 17:32:38 +0430

    sortClasses

--- src/main/java/controller/sortingAndFiltering/ControllerForSorting.java
+++ src/main/java/controller/sortingAndFiltering/ControllerForSorting.java
@@ -1,4 +1,30 @@
 package controller.sortingAndFiltering;
 
+import model.productThings.Good;
+
+import java.util.Comparator;
+
 public class ControllerForSorting{
+
+    class SortByVisitNumber implements Comparator<Good>{
+        public int compare(Good firstGood, Good secondGood){
+            return secondGood.getSeenNumber() - firstGood.getSeenNumber();
+        }
+    }
+
+    class SortByAverageRate implements Comparator<Good>{
+        public int compare(Good firstGood, Good secondGood){
+            if (secondGood.getAverageRate() - firstGood.getAverageRate() > 0)
+                return 1;
+            return (-1) ;
+        }
+    }
+
+    class SortByDate implements Comparator<Good>{
+        public int compare(Good firstGood, Good secondGood){
+            if (secondGood.getModificationDate().isAfter(secondGood.getModificationDate()))
+                return 1;
+            return (-1);
+        }
+    }
 }
