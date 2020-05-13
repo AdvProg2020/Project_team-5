9b37240aee7bd0141bd3248f0ae1a963494a6950
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-05-14 00:16:33 +0430

    create signature of methods of SavingData

--- src/main/java/model/database/SavingData.java
+++ src/main/java/model/database/SavingData.java
@@ -1,4 +1,51 @@
 package model.database;
 
 public class SavingData {
+    public void saveManager() {
+
+    }
+
+    public void saveCustomer() {
+
+    }
+
+    public void saveSeller() {
+
+    }
+
+    public void saveProduct() {
+
+    }
+
+    public void saveDiscount() {
+
+    }
+
+    public void saveComment() {
+
+    }
+
+    public void saveOff() {
+
+    }
+
+    public void saveRate() {
+
+    }
+
+    public void saveCategory() {
+
+    }
+
+    public void saveSubCategory() {
+
+    }
+
+    public void saveOrderForSeller() {
+
+    }
+
+    public void saveOrderForCustomer() {
+
+    }
 }
