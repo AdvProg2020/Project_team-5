cb6fc8f8f2a56365500f3e4bf889b74c64fcf27c
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-05-15 02:20:02 +0430

    load files part-1

--- src/main/java/model/database/LoadingData.java
+++ src/main/java/model/database/LoadingData.java
@@ -1,8 +1,27 @@
 package model.database;
 
-public class LoadingData {
-    public void loadManager() {
+import com.gilecode.yagson.YaGson;
+import model.Shop;
+import model.persons.Manager;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+
+public class LoadingData {
+    private FileReader fileReader;
+    private YaGson yaGson;
+
+    public LoadingData() {
+        yaGson = new YaGson();
+    }
+
+    public void loadManager() throws IOException {
+        File[] files = loadFolder("Resources\\Users\\Managers");
+        for (File file : files) {
+            Shop.getInstance().addPerson(yaGson.fromJson(readFile(file), Manager.class));
+        }
     }
 
     public void loadCustomer() {
@@ -51,5 +70,16 @@ public class LoadingData {
 
     public void loadRequests() {
 
+    }
+
+    private File[] loadFolder(String folderPath) throws IOException {
+        File file = new File(folderPath);
+        if (!file.exists())
+            file.createNewFile();
+        return file.listFiles();
+    }
+
+    private String readFile(File file) throws IOException {
+        return Files.readString(file.toPath());
     }
 }
