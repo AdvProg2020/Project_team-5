c26dd67e24e6773eeaaca4efc8a01f8f6e338cff
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-05-14 14:24:23 +0430

    save file part-2 (set file paths)

--- src/main/java/model/database/SavingData.java
+++ src/main/java/model/database/SavingData.java
@@ -17,55 +17,59 @@ import java.io.IOException;
 
 public class SavingData {
     public void saveManager(Manager manager) {
-
+        String filePath = "Resources\\Users\\Managers\\" + manager.getUsername() + ".json";
     }
 
     public void saveCustomer(Customer customer) {
-
+        String filePath = "Resources\\Users\\Customers\\" + customer.getUsername() + ".json";
     }
 
     public void saveSeller(Seller seller) {
-
+        String filePath = "Resources\\Users\\Sellers\\" + seller.getUsername() + ".json";
     }
 
     public void saveProduct(Good good) {
-
+        String filePath = "Resources\\Products\\product_" + good.getGoodId() + ".json";
     }
 
     public void saveDiscount(DiscountCode discountCode) {
+        String filePath = "Resources\\Discounts\\dis_" + discountCode.getCode() + ".json";
+    }
 
+    public void saveInfoAboutGood(SellerRelatedInfoAboutGood infoAboutGood, long goodId) {
+        String filePath = "Resources\\ProductsInfo\\productInfo_" + goodId + "_" + infoAboutGood.getSeller().getUsername() + ".json";
     }
 
     public void saveComment(Comment comment) {
-
+        String filePath = "Resources\\Comments\\comment_" + comment.getGood().getGoodId() + "_" + comment.getPerson().getUsername() + ".json";
     }
 
     public void saveOff(Off off) {
-
+        String filePath = "Resources\\Offs\\off_" + off.getOffId() + ".json";
     }
 
     public void saveRate(Rate rate) {
-
+        String filePath = "Resources\\Rates\\rate_" + rate.getGood().getGoodId() + "_" + rate.getCustomer().getUsername() + ".json";
     }
 
     public void saveCategory(Category category) {
-
+        String filePath = "Resources\\Categories\\" + category.getName() + ".json";
     }
 
     public void saveSubCategory(SubCategory subCategory) {
-
+        String filePath = "Resources\\SubCategories\\" + subCategory.getParentCategory().getName() + "_" + subCategory.getName() + ".json";
     }
 
     public void saveOrderForSeller(OrderForSeller orderForSeller) {
-
+        String filePath = "Resources\\Orders\\OrderForSellers\\order_" + orderForSeller.getOrderId() + ".json";
     }
 
     public void saveOrderForCustomer(OrderForCustomer orderForCustomer) {
-
+        String filePath = "Resources\\Orders\\OrderForCustomers\\order_" + orderForCustomer.getOrderId() + ".json";
     }
 
     public void saveRequest(Request request) {
-
+        String filePath = "Resources\\Requests\\request_" + request.getRequestId() + ".json";
     }
 
     private void saveFile(String serializedData, String filePath) throws IOException, FileCantBeSavedException {
