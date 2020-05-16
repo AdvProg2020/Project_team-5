d1ffb25ad8cd50563d5f26179d9bd1d07efb4aca
ASUS <yasamanzolfi4@gmail.com>
2020-05-16 21:24:38 +0430

    debug

--- src/main/java/view/Menu.java
+++ src/main/java/view/Menu.java
@@ -61,8 +61,10 @@ public abstract class Menu {
             if (Pattern.matches("[0-9]+", input))
                 if (Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= commandNames.size() + submenus.size() + 1)
                     return Integer.parseInt(input);
+            System.out.println(Integer.parseInt(input) >= 1 && Integer.parseInt(input) <= commandNames.size() + submenus.size() + 1);
             System.out.println("not valid input. please try again");
         }
+
     }
 
     protected String getValidInput(String regex, String error) {
