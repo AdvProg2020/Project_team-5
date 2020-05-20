1e845b21227df12b612fad3a74bf3934206b621c
aboots <mahid.abootorabi2@gmail.com>
2020-05-20 21:43:18 +0430

    clear shop!

---
+++ src/test/java/TestShop.java
@@ -0,0 +1,19 @@
+import model.Shop;
+
+public class TestShop {
+    public static void clearShop(){
+        Shop.getInstance().getHashMapOfCategories().clear();
+        Shop.getInstance().getHashMapOfOffs().clear();
+        Shop.getInstance().getAllPersons().clear();
+        Shop.getInstance().getAllRequest().clear();
+        Shop.getInstance().getHashMapOfDiscountCodes().clear();
+        Shop.getInstance().getAllRates().clear();
+        Shop.getInstance().getAllGoodInCarts().clear();
+        Shop.getInstance().getHashMapOfGoods().clear();
+        Shop.getInstance().getHasMapOfOrders().clear();
+        Shop.getInstance().getAllSubCategories().clear();
+        Shop.getInstance().getAllComments().clear();
+        Shop.getInstance().getAllCompanies().clear();
+        Shop.getInstance().getAllSellerRelatedInfoAboutGood().clear();
+    }
+}
