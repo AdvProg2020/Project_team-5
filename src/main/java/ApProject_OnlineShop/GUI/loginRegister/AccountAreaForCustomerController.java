1539036fbba55bb0e43ce69d542ebd2c58af2069
ASUS <yasamanzolfi4@gmail.com>
2020-06-07 20:02:33 +0430

    accountAreaForCustomerController editing

---
+++ src/main/java/ApProject_OnlineShop/GUI/loginRegister/AccountAreaForCustomerController.java
@@ -0,0 +1,11 @@
+package ApProject_OnlineShop.GUI.loginRegister;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AccountAreaForCustomerController {
+    public Label userName;
+    public Label name;
+    public Label lastName;
+    public Label email;
+}
