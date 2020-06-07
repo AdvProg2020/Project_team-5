91a7a5de218d566d3aede3f3e70920a601bbc195
ASUS <yasamanzolfi4@gmail.com>
2020-06-07 17:37:50 +0430

    accountAreaForCustomer

---
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/AccountAreaForCustomerController.java
@@ -0,0 +1,16 @@
+package ApProject_OnlineShop.GUI.accountArea;
+
+import javafx.scene.control.Label;
+
+public class AccountAreaForCustomerController {
+
+//    public Label userName;
+//    public Label name;
+//    public Label lastName;
+//    public Label email;
+//
+//    public AccountAreaForCustomerController() {
+//        userName.setText("yasaman");
+//        lastName.setText("zolfi");
+//    }
+}
