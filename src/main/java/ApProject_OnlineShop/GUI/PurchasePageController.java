c5e12e097b526ed3c9daf502f488e74572da7b68
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-08 21:00:49 +0430

    purchase controller creating methods

--- src/main/java/ApProject_OnlineShop/GUI/PurchasePageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/PurchasePageController.java
@@ -1,12 +1,16 @@
 package ApProject_OnlineShop.GUI;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class PurchasePageController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PurchasePageController implements Initializable {
     @FXML
     private Label shouldPayLabel;
     @FXML
@@ -29,4 +33,37 @@ public class PurchasePageController {
     private TextField phoneNumberField;
     @FXML
     private TextField discountCodeField;
+
+    public void onBackButtonPressed() {
+
+    }
+
+    public void onSubmitButtonPressed() {
+        String name = nameField.getText();
+        String postalCode = postalCodeField.getText();
+        String address = addressField.getText();
+        String phoneNumber = phoneNumberField.getText();
+        if (name.isEmpty() || address.isEmpty() || postalCode.isEmpty() || phoneNumber.isEmpty()) {
+            //TODO
+        }
+        /*
+        write logic
+         */
+        applyDiscountButton.setDisable(false);
+        purchaseButton.setDisable(false);
+    }
+
+    public void onApplyDiscountButtonPressed() {
+
+    }
+
+    public void onPurchaseButtonPressed() {
+        //TODO
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        applyDiscountButton.setDisable(true);
+        purchaseButton.setDisable(true);
+    }
 }
