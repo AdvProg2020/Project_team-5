39d3948ade8419d20d0773e2cbf7584688f5f7e9
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-08 20:07:35 +0430

    purchase controller creating fields

--- src/main/java/ApProject_OnlineShop/GUI/PurchasePageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/PurchasePageController.java
@@ -1,4 +1,32 @@
 package ApProject_OnlineShop.GUI;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class PurchasePageController {
+    @FXML
+    private Label shouldPayLabel;
+    @FXML
+    private Label totalPriceLabel;
+    @FXML
+    private Button purchaseButton;
+    @FXML
+    private Button applyDiscountButton;
+    @FXML
+    private Button submitPropertiesButton;
+    @FXML
+    private Button backButton;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField postalCodeField;
+    @FXML
+    private TextArea addressField;
+    @FXML
+    private TextField phoneNumberField;
+    @FXML
+    private TextField discountCodeField;
 }
