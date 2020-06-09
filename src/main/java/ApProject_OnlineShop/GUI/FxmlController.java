b7cfd1bc178e663e8cb9f0d5a509407891c13690
aboots <mahid.abootorabi2@gmail.com>
2020-06-10 01:13:33 +0430

    add logout icon to seller account area

--- src/main/java/ApProject_OnlineShop/GUI/FxmlController.java
+++ src/main/java/ApProject_OnlineShop/GUI/FxmlController.java
@@ -4,9 +4,12 @@ import ApProject_OnlineShop.Main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Optional;
 
 public class FxmlController {
 
@@ -20,8 +23,16 @@ public class FxmlController {
         }
         stage.setTitle(title);
         stage.setScene(new Scene(root, 1000, 800));
-       // stage.setMaximized(true);
+        // stage.setMaximized(true);
         stage.show();
+    }
+
+    public Optional<ButtonType> showAlert(Alert.AlertType alertType, String title, String header, String contentText) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(header);
+        alert.setContentText(contentText);
+        return alert.showAndWait();
     }
 
 }
--- src/main/java/ApProject_OnlineShop/GUI/accountArea/AccountAreaForSellerController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/AccountAreaForSellerController.java
@@ -2,12 +2,16 @@ package ApProject_OnlineShop.GUI.accountArea;
 
 import ApProject_OnlineShop.GUI.FxmlController;
 import ApProject_OnlineShop.controller.MainController;
+import ApProject_OnlineShop.model.Shop;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AccountAreaForSellerController extends FxmlController implements Initializable {
@@ -44,4 +48,15 @@ public class AccountAreaForSellerController extends FxmlController implements In
 
     public void viewBalance(MouseEvent mouseEvent) {
     }
+
+    public void logout(MouseEvent mouseEvent) {
+        Optional<ButtonType> result = showAlert
+                (Alert.AlertType.CONFIRMATION, "Logout", "Logout", "are you sure to logout?");
+        if (result.get() == ButtonType.OK) {
+            MainController.getInstance().getLoginRegisterController().logoutUser();
+            Shop.getInstance().clearCart();
+            setScene("mainMenuLayout.fxml", "Main menu");
+        }
+    }
+
 }
