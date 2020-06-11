d9d72527f190c72f2ca495b442c58430c454af64
aboots <mahid.abootorabi2@gmail.com>
2020-06-11 20:34:04 +0430

    work on editField

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/EditFieldPersonsController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/EditFieldPersonsController.java
@@ -1,8 +1,10 @@
 package ApProject_OnlineShop.GUI.accountArea;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.controller.MainController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
@@ -10,14 +12,26 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Font;
 
-public class EditFieldPersonsController extends FxmlController {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class EditFieldPersonsController extends FxmlController implements Initializable {
     @FXML
     Label passwordLabel;
     @FXML
     PasswordField password;
     @FXML
-    TextField username, email, phoneNumber, firstName, lastName;
+    TextField email, phoneNumber, firstName, lastName;
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        ArrayList<String> personalInfo = MainController.getInstance().getAccountAreaForSellerController().getUserPersonalInfo();
+        firstName.setPromptText(personalInfo.get(1));
+        lastName.setPromptText(personalInfo.get(2));
+        email.setPromptText(personalInfo.get(3));
+        phoneNumber.setPromptText(personalInfo.get(4));
+    }
 
     public void PasswordHint(MouseEvent mouseEvent) {
         Tooltip tooltip = new Tooltip("-must contains one digit from 0-9\n" +
@@ -28,9 +42,6 @@ public class EditFieldPersonsController extends FxmlController {
         passwordLabel.setTooltip(tooltip);
     }
 
-    public void RegisterForManagerPressed(ActionEvent actionEvent) {
-
-    }
 
     public void backButtonAction(ActionEvent actionEvent) {
 
@@ -38,5 +49,8 @@ public class EditFieldPersonsController extends FxmlController {
 
     public void logout(MouseEvent mouseEvent) {
 
+    }
+
+    public void editPressed(ActionEvent actionEvent) {
     }
 }
