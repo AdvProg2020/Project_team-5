f1cf83fc9b48683f4fb82c9534dfbc803b7121d1
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-12 02:49:38 +0430

    view discounts page back and logout

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/ViewDiscountCodesPageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/ViewDiscountCodesPageController.java
@@ -1,14 +1,33 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForManager;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.controller.MainController;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ViewDiscountCodesPageController extends FxmlController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void onLogoutIconClicked() {
+        Optional<ButtonType> result = showAlert
+                (Alert.AlertType.CONFIRMATION, "Logout", "Logout", "are you sure to logout?");
+        if (result.get() == ButtonType.OK) {
+            MainController.getInstance().getLoginRegisterController().logoutUser();
+            setScene("mainMenuLayout.fxml", "Main menu");
+        }
+    }
+
+    public void onBackButtonPressed() {
+        setScene("accountAreaForManager.fxml", "account area");
     }
 }
