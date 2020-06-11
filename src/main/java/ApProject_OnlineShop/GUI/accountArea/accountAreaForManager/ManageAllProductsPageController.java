bbd76571d14252e9242b214951592eb2438d74d9
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-12 02:15:14 +0430

    manage products page basics

---
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/ManageAllProductsPageController.java
@@ -0,0 +1,14 @@
+package ApProject_OnlineShop.GUI.accountArea.accountAreaForManager;
+
+import ApProject_OnlineShop.GUI.FxmlController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ManageAllProductsPageController extends FxmlController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
