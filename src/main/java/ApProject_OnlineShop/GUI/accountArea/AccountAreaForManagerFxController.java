e08fb82def40b8dda00acda9ea7e8e00d4889f20
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-11 13:34:40 +0430

    accountAreaForManager fxml creating

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/AccountAreaForManagerFxController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/AccountAreaForManagerFxController.java
@@ -1,12 +1,32 @@
 package ApProject_OnlineShop.GUI.accountArea;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.controller.MainController;
+import ApProject_OnlineShop.model.Shop;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AccountAreaForManagerFxController extends FxmlController implements Initializable {
+
+    public void onBackButtonPressed() {
+        setScene("mainMenuLayout.fxml","main menu");
+    }
+
+    public void onLogoutClicked(MouseEvent mouseEvent) {
+        Optional<ButtonType> result = showAlert
+                (Alert.AlertType.CONFIRMATION, "Logout", "Logout", "are you sure to logout?");
+        if (result.get() == ButtonType.OK) {
+            MainController.getInstance().getLoginRegisterController().logoutUser();
+            setScene("mainMenuLayout.fxml", "Main menu");
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
