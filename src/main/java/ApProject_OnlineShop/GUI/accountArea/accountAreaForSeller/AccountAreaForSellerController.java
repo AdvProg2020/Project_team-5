0baf41fea7a58d8be388042e9e671b3fb623492f
aboots <mahid.abootorabi2@gmail.com>
2020-06-12 02:20:29 +0430

    work on view offs

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/AccountAreaForSellerController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/AccountAreaForSellerController.java
@@ -48,9 +48,7 @@ public class AccountAreaForSellerController extends FxmlController implements In
     }
 
     public void viewOffs(MouseEvent mouseEvent) {
-    }
-
-    public void viewBalance(MouseEvent mouseEvent) {
+        setScene("viewOffsForSller.fxml", "Offs");
     }
 
     public void logout(MouseEvent mouseEvent) {
---
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/ViewOffsController.java
@@ -0,0 +1,32 @@
+package ApProject_OnlineShop.GUI.accountArea.accountAreaForSeller;
+
+import ApProject_OnlineShop.GUI.FxmlController;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewOffsController extends FxmlController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+    public void backButton(ActionEvent actionEvent) {
+    }
+
+    public void logout(MouseEvent mouseEvent) {
+    }
+
+    public void sortByEndDate(ActionEvent actionEvent) {
+    }
+
+    public void sortByOffPercent(ActionEvent actionEvent) {
+    }
+
+    public void sortByMaxDiscount(ActionEvent actionEvent) {
+    }
+}
