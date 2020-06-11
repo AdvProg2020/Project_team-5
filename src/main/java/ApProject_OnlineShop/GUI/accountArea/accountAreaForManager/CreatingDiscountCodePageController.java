74af7ecf2efb310cad10ddd729b83fc697ea7f4d
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-11 21:29:45 +0430

    creating discountCode logout

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/CreatingDiscountCodePageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/CreatingDiscountCodePageController.java
@@ -1,16 +1,30 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForManager;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.controller.MainController;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class CreatingDiscountCodePageController extends FxmlController implements Initializable {
 
     public void onBackButtonPressed() {
-
+        setScene("accountAreaForManager.fxml", "account area");
     }
+
+    public void onLogoutIconClicked() {
+        Optional<ButtonType> result = showAlert
+                (Alert.AlertType.CONFIRMATION, "Logout", "Logout", "are you sure to logout?");
+        if (result.get() == ButtonType.OK) {
+            MainController.getInstance().getLoginRegisterController().logoutUser();
+            setScene("mainMenuLayout.fxml", "Main menu");
+        }
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
