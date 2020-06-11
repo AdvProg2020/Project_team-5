e77287d2ea40a40045f26b3f946b4242fea12690
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-11 13:46:43 +0430

    accountAreaForManager fxml Controller creating

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/AccountAreaForManagerFxController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/AccountAreaForManagerFxController.java
@@ -3,9 +3,11 @@ package ApProject_OnlineShop.GUI.accountArea;
 import ApProject_OnlineShop.GUI.FxmlController;
 import ApProject_OnlineShop.controller.MainController;
 import ApProject_OnlineShop.model.Shop;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
@@ -13,6 +15,16 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AccountAreaForManagerFxController extends FxmlController implements Initializable {
+    @FXML
+    private Label userName;
+    @FXML
+    private Label name;
+    @FXML
+    private Label lastName;
+    @FXML
+    private Label email;
+    @FXML
+    private Label phoneNumber;
 
     public void onBackButtonPressed() {
         setScene("mainMenuLayout.fxml","main menu");
@@ -25,6 +37,30 @@ public class AccountAreaForManagerFxController extends FxmlController implements
             MainController.getInstance().getLoginRegisterController().logoutUser();
             setScene("mainMenuLayout.fxml", "Main menu");
         }
+    }
+
+    public void onViewDiscountCodesPressed() {
+
+    }
+
+    public void onManageUsersPressed() {
+
+    }
+
+    public void onManageRequestsPressed() {
+
+    }
+
+    public void onManageAllProductsPressed() {
+
+    }
+
+    public void onManageCategoriesPressed() {
+
+    }
+
+    public void onCreateNewDiscountPressed() {
+
     }
 
     @Override
