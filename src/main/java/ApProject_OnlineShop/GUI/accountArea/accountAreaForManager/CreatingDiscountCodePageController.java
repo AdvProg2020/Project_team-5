8b468221efc91f9e80670c3d09a2cf6ddfec70da
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-11 22:14:43 +0430

    completing design of discountCode page

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/CreatingDiscountCodePageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/CreatingDiscountCodePageController.java
@@ -2,15 +2,28 @@ package ApProject_OnlineShop.GUI.accountArea.accountAreaForManager;
 
 import ApProject_OnlineShop.GUI.FxmlController;
 import ApProject_OnlineShop.controller.MainController;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
 
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class CreatingDiscountCodePageController extends FxmlController implements Initializable {
+    @FXML
+    private TextField discountPercentTextField;
+    @FXML
+    private TextField discountAmountTextField;
+    @FXML
+    private TextField codeTextField;
+    @FXML
+    private DatePicker startDateChooser;
+    @FXML
+    private DatePicker endDateChooser;
 
     public void onBackButtonPressed() {
         setScene("accountAreaForManager.fxml", "account area");
@@ -23,6 +36,10 @@ public class CreatingDiscountCodePageController extends FxmlController implement
             MainController.getInstance().getLoginRegisterController().logoutUser();
             setScene("mainMenuLayout.fxml", "Main menu");
         }
+    }
+
+    public void onCreateDiscountCodePressed() {
+
     }
 
     @Override
