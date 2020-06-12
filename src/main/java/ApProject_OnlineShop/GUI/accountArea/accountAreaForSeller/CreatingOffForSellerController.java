1e0b02a44d087efbced56ff71ac167b948ea0fa1
aboots <mahid.abootorabi2@gmail.com>
2020-06-12 15:47:48 +0430

    work on create discountcode!

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/CreatingOffForSellerController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/CreatingOffForSellerController.java
@@ -1,6 +1,8 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForSeller;
 
+import ApProject_OnlineShop.GUI.ErrorPageFxController;
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.GUI.SuccessPageFxController;
 import ApProject_OnlineShop.controller.MainController;
 import ApProject_OnlineShop.model.Shop;
 import javafx.event.ActionEvent;
@@ -11,6 +13,9 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Optional;
 
 public class CreatingOffForSellerController extends FxmlController {
@@ -22,15 +27,43 @@ public class CreatingOffForSellerController extends FxmlController {
     @FXML
     public TextField productToAdd;
 
+    private ArrayList<Long> offProducts = new ArrayList<>();
+    private boolean finished;
+
     public void onBackButtonPressed(ActionEvent actionEvent) {
         setScene("viewOffsForSeller.fxml", "Offs");
     }
 
     public void onCreateDiscountCodePressed(ActionEvent actionEvent) {
-
+        if (checkInfos()) {
+            if (!finished) {
+                ErrorPageFxController.showPage("error for create off", "you should add products and press finished then create discount code");
+            } else {
+                ArrayList<String> offDetails = new ArrayList<>();
+                offDetails.add(startDateChooser.getValue().toString());
+                offDetails.add(endDateChooser.getValue().toString());
+                offDetails.add(maxAmount.getText());
+                offDetails.add(discountPercentTextField.getText());
+                try {
+                    MainController.getInstance().getAccountAreaForSellerController().addOff(offDetails, offProducts);
+                    SuccessPageFxController.showPage("off request created successfully", "your request successfully sent to manager");
+                    offProducts.clear();
+                    finished = false;
+                } catch (Exception e) {
+                    ErrorPageFxController.showPage("off request cannot be created", e.getMessage());
+                    finished = false;
+                    offProducts.clear();
+                }
+            }
+        }
     }
 
     public void onFinishButtonPressed(ActionEvent actionEvent) {
+        if (offProducts.size() == 0) {
+            ErrorPageFxController.showPage("Error for adding products", "you should add at least one prodcut to this off");
+        } else {
+            finished = true;
+        }
     }
 
     public void onLogoutIconClicked(MouseEvent mouseEvent) {
@@ -44,5 +77,37 @@ public class CreatingOffForSellerController extends FxmlController {
     }
 
     public void onAddProductPressed(ActionEvent actionEvent) {
+        if (!productToAdd.getText().matches("\\d+")) {
+            ErrorPageFxController.showPage("error for adding product", "you should enter a product number");
+            productToAdd.clear();
+            return;
+        }
+        if (!MainController.getInstance().getAccountAreaForSellerController().checkValidProductId(Long.parseLong(productToAdd.getText()))) {
+            ErrorPageFxController.showPage("error for adding product", "This product does not exist your active goods");
+            productToAdd.clear();
+        } else {
+            offProducts.add(Long.parseLong(productToAdd.getText()));
+        }
+    }
+
+    private boolean checkInfos() {
+        if (!maxAmount.getText().matches("\\d\\d\\d\\d+")) {
+            ErrorPageFxController.showPage("error for create off", "max amount discount format not valid!");
+            return false;
+        } else if (!discountPercentTextField.getText().matches("[\\d]{1,2}")) {
+            ErrorPageFxController.showPage("error for create off", "discount percent amount discount format not valid!");
+            return false;
+        } else if (!startDateChooser.getValue().isAfter(LocalDate.now())) {
+            ErrorPageFxController.showPage("error for create off", "start date should be after now!");
+            return false;
+        } else if (!endDateChooser.getValue().isAfter(LocalDate.now())) {
+            ErrorPageFxController.showPage("error for create off", "end date should be after now!");
+            return false;
+        } else if (!endDateChooser.getValue().isAfter(startDateChooser.getValue())) {
+            ErrorPageFxController.showPage("error for create off", "end date should be after start date!");
+            return false;
+        } else {
+            return true;
+        }
     }
 }
