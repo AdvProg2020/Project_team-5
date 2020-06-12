34b98446db456f3a7ca6f5124c7caae4538b4e90
aboots <mahid.abootorabi2@gmail.com>
2020-06-13 02:41:07 +0430

    add icon

--- src/main/java/ApProject_OnlineShop/GUI/StageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/StageController.java
@@ -35,7 +35,7 @@ public class StageController {
         }
         stage.setTitle("Shop");
         stage.setScene(new Scene(root, 1000, 800));
-        stage.getIcons().add(new Image(getClass().getClassLoader().getResource("pictures/icon.jpg").toString()));
+        stage.getIcons().add(new Image(getClass().getClassLoader().getResource("pictures/iconcopy.png").toString()));
         //       stage.setMaximized(true);
         stage.show();
     }
