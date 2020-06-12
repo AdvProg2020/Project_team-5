392754a889c1f65a02c7cbcdc47f5ae21e49283e
aboots <mahid.abootorabi2@gmail.com>
2020-06-12 14:23:39 +0430

    back and logout for off page

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/ViewOffsController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/ViewOffsController.java
@@ -2,16 +2,20 @@ package ApProject_OnlineShop.GUI.accountArea.accountAreaForSeller;
 
 import ApProject_OnlineShop.GUI.FxmlController;
 import ApProject_OnlineShop.controller.MainController;
+import ApProject_OnlineShop.model.Shop;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ViewOffsController extends FxmlController implements Initializable {
@@ -48,13 +52,21 @@ public class ViewOffsController extends FxmlController implements Initializable 
     private void viewSingleOff(String off) {
         long id = Long.valueOf(off.substring(8, off.indexOf("\t")));
         ViewSpecificOffController.setOffId(id);
-        setScene("showSpecificOffForSeller.fxml","Off");
+        setScene("showSpecificOffForSeller.fxml", "Off");
     }
 
     public void backButton(ActionEvent actionEvent) {
+        setScene("accountAreaForSeller.fxml", "account area");
     }
 
     public void logout(MouseEvent mouseEvent) {
+        Optional<ButtonType> result = showAlert
+                (Alert.AlertType.CONFIRMATION, "Logout", "Logout", "are you sure to logout?");
+        if (result.get() == ButtonType.OK) {
+            MainController.getInstance().getLoginRegisterController().logoutUser();
+            Shop.getInstance().clearCart();
+            setScene("mainMenuLayout.fxml", "Main menu");
+        }
     }
 
     public void sortByEndDate(ActionEvent actionEvent) {
@@ -73,5 +85,6 @@ public class ViewOffsController extends FxmlController implements Initializable 
     }
 
     public void addOff(MouseEvent mouseEvent) {
+
     }
 }
