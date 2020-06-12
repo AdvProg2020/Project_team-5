3280d665dbd83ea4b90131b645d50225858bfdaf
aboots <mahid.abootorabi2@gmail.com>
2020-06-12 21:45:58 +0430

    work on add a  product

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/AddProductPart2.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/AddProductPart2.java
@@ -1,17 +1,26 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForSeller;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.GUI.StageController;
+import ApProject_OnlineShop.model.productThings.Good;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class AddProductPart2 extends FxmlController implements Initializable {
 
     private static ArrayList<String> productDetails;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -32,5 +41,17 @@ public class AddProductPart2 extends FxmlController implements Initializable {
 
     public static void setProductDetails(ArrayList<String> productDetails) {
         AddProductPart2.productDetails = productDetails;
+    }
+
+    public void selectPhoto(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(StageController.getStage());
+        try {
+            java.nio.file.Files.copy(selectedFile.toPath(), Paths.get("src/"));
+            Files.copy(selectedFile.toPath(),
+                    (new File("./resources/productImages/" + Good.getGoodsCount() + ".png")).toPath(), StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
