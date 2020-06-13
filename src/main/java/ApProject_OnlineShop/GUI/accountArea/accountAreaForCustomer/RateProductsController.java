d40a537ce4f5b9f090c902fc5b9d8bd973697ba9
aboots <mahid.abootorabi2@gmail.com>
2020-06-13 19:48:00 +0430

    work on rate products

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForCustomer/RateProductsController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForCustomer/RateProductsController.java
@@ -1,23 +1,43 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForCustomer;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.GUI.ProductPageRelated.ProductBriefSummery;
 import ApProject_OnlineShop.controller.MainController;
 import ApProject_OnlineShop.model.Shop;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
+import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class RateProductsController extends FxmlController implements Initializable {
-    
+    @FXML
+    public GridPane root;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+//       List<Long> productIds = MainController.getInstance().getAccountAreaForSellerController().viewProducts(sortSelected);
+//        int num = 0;
+//        int row = 0;
+//        for (Long productId : productIds) {
+//            if (MainController.getInstance().getAccountAreaForSellerController().isInOff(productId)) {
+//                root.add(new ProductBriefSummery().offProductBriefSummery(productId), num % 3, row);
+//                num++;
+//            }
+//            if (!MainController.getInstance().getAccountAreaForSellerController().isInOff(productId)) {
+//                root.add(new ProductBriefSummery().getProductForAllProductsPage(productId), num % 3, row);
+//                num++;
+//            }
+//            if (num % 3 == 0)
+//                row++;
+//        }
     }
 
     public void backButton(ActionEvent actionEvent) {
