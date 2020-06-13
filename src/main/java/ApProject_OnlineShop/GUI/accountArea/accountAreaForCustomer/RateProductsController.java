1706d8e0505710b7fd78aefca42a38a085239610
aboots <mahid.abootorabi2@gmail.com>
2020-06-13 20:32:44 +0430

    work on rate products

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForCustomer/RateProductsController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForCustomer/RateProductsController.java
@@ -2,42 +2,71 @@ package ApProject_OnlineShop.GUI.accountArea.accountAreaForCustomer;
 
 import ApProject_OnlineShop.GUI.FxmlController;
 import ApProject_OnlineShop.GUI.ProductPageRelated.ProductBriefSummery;
+import ApProject_OnlineShop.GUI.SuccessPageFxController;
 import ApProject_OnlineShop.controller.MainController;
 import ApProject_OnlineShop.model.Shop;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.List;
-import java.util.Optional;
-import java.util.ResourceBundle;
+import java.util.*;
 
 public class RateProductsController extends FxmlController implements Initializable {
     @FXML
     public GridPane root;
+    private HashMap<VBox, Long> productDetails = new HashMap<>();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-//       List<Long> productIds = MainController.getInstance().getAccountAreaForSellerController().viewProducts(sortSelected);
-//        int num = 0;
-//        int row = 0;
-//        for (Long productId : productIds) {
-//            if (MainController.getInstance().getAccountAreaForSellerController().isInOff(productId)) {
-//                root.add(new ProductBriefSummery().offProductBriefSummery(productId), num % 3, row);
-//                num++;
-//            }
-//            if (!MainController.getInstance().getAccountAreaForSellerController().isInOff(productId)) {
-//                root.add(new ProductBriefSummery().getProductForAllProductsPage(productId), num % 3, row);
-//                num++;
-//            }
-//            if (num % 3 == 0)
-//                row++;
-//        }
+        List<Long> productIds = MainController.getInstance().getAccountAreaForCustomerController().getBoughtProducts();
+        int num = 0;
+        int row = 0;
+        for (Long productId : productIds) {
+            VBox vbox = null;
+            if (MainController.getInstance().getAccountAreaForSellerController().isInOff(productId)) {
+                vbox = new ProductBriefSummery().offProductBriefSummery(productId);
+            }
+            if (!MainController.getInstance().getAccountAreaForSellerController().isInOff(productId)) {
+                vbox = new ProductBriefSummery().getProductForAllProductsPage(productId);
+            }
+            root.add(vbox, num % 3, row);
+            num++;
+            productDetails.put(vbox, productId);
+            vbox.setOnMouseClicked(e -> rateProduct());
+            vbox.setCursor(Cursor.HAND);
+            if (num % 3 == 0)
+                row++;
+        }
+    }
+
+    private void rateProduct() {
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setResizable(false);
+        window.setTitle("Rate");
+        Parent root = null;
+        try {
+            root = FXMLLoader.load(Objects.requireNonNull(SuccessPageFxController.class.getClassLoader().getResource("successPage.fxml")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(root, 600, 400);
+        window.setScene(scene);
+        window.showAndWait();
     }
 
     public void backButton(ActionEvent actionEvent) {
