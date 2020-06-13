c70186888498b1a12c5e57f7d439e4f37b41b473
ASUS <yasamanzolfi4@gmail.com>
2020-06-13 12:13:57 +0430

    work on product summery in all product page

--- src/main/java/ApProject_OnlineShop/GUI/ProductPageRelated/ProductBriefSummery.java
+++ src/main/java/ApProject_OnlineShop/GUI/ProductPageRelated/ProductBriefSummery.java
@@ -11,66 +11,59 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 import java.awt.*;
 
 public class ProductBriefSummery {
-    public VBox getProductForAllProductsPage(){
+    public VBox getProductForAllProductsPage(int productId) {
         VBox mainVBox = new VBox();
         setStyleForVBox(mainVBox);
         mainVBox.setAlignment(Pos.CENTER);
-        ImageView imageView = new ImageView(getClass().getClassLoader().getResource("pictures/7.jpg").toString());
+        ImageView imageView = new ImageView(getClass().getClassLoader().getResource("pictures/" + productId + ".jpg").toString());
         VBox image = new VBox();
         image.getChildren().add(imageView);
         mainVBox.getChildren().add(image);
-        image.setMaxSize(150,150);
+        image.setMaxSize(150, 150);
         imageView.setFitHeight(150);
         imageView.setFitWidth(150);
         VBox nameVBox = new VBox();
         nameVBox.setAlignment(Pos.CENTER_LEFT);
         Label name = new Label("Galaxy S9 Samsung");
-        (new Styles()).setTextFont(name);
-        name.setPadding(new Insets(0,15,0,15));
+        name.setFont(Font.font("Times New Roman", 16));
+        name.setPadding(new Insets(0, 15, 0, 15));
         nameVBox.getChildren().add(name);
         mainVBox.getChildren().add(nameVBox);
         HBox rateHBox = new HBox();
         rateHBox.setAlignment(Pos.CENTER_LEFT);
-        Label rate = new Label("8.0");
-        rate.setPadding(new Insets(0,0,0,0));
-        rateHBox.setPadding(new Insets(0,15,0,8));
-        ImageView star = new ImageView(new Image(getClass().getClassLoader().getResource("pictures/star.png").toString()));
-        star.setFitWidth(20);
-        star.setFitHeight(20);
-        rateHBox.getChildren().add(star);
+        Label rate = new Label("4.0");
+        rateHBox.setPadding(new Insets(0, 15, 0, 15));
+        for (int i = 0; i < 4; i++) {
+            ImageView star = new ImageView(new Image(getClass().getClassLoader().getResource("pictures/star.png").toString()));
+            star.setFitWidth(20);
+            star.setFitHeight(20);
+            rateHBox.getChildren().add(star);
+        }
         rateHBox.getChildren().add(rate);
         mainVBox.getChildren().add(rateHBox);
-        (new Styles()).setTextFont(rate);
-        rate.setPadding(new Insets(4,0,0,4));
+        rate.setFont(Font.font("Times New Roman", 16));
         VBox priceVBox = new VBox();
         priceVBox.setAlignment(Pos.CENTER_LEFT);
-        priceVBox.setPadding(new Insets(0,15,0,15));
+        priceVBox.setPadding(new Insets(0, 15, 0, 15));
         Label price = new Label("9000" + " Rial");
         priceVBox.getChildren().add(price);
         mainVBox.getChildren().add(priceVBox);
-        (new Styles()).setTextFont(price);
-        price.setPadding(new Insets(0,0,0,0));
+        price.setFont(Font.font("Times New Roman", 16));
         return mainVBox;
     }
 
-    private void setStyleForVBox(VBox gridPane){
+    private void setStyleForVBox(VBox gridPane) {
         gridPane.setStyle("-fx-border-color:#8600b3;" +
                 "-fx-border-width: 1;" +
-                "-fx-border-style: solid;"+
+                "-fx-border-style: solid;" +
                 "-fx-background-color: #ffffff;");
-        gridPane.setMaxSize(250,250);
-        gridPane.setMinSize(250,250);
+        gridPane.setMaxSize(250, 250);
+        gridPane.setMinSize(250, 250);
         gridPane.setSpacing(8);
-    }
-
-    public void setScene(){
-        GridPane gridPane = new GridPane();
-        gridPane.setAlignment(Pos.CENTER);
-        gridPane.add(getProductForAllProductsPage(),0,0);
-        StageController.setSceneJavaFx(gridPane);
     }
 }
