599f7cfbcacecb3742089310ff77799e28687c29
ASUS <yasamanzolfi4@gmail.com>
2020-06-14 01:28:38 +0430

    product page

--- src/main/java/ApProject_OnlineShop/GUI/productPageRelated/ProductPage.java
+++ src/main/java/ApProject_OnlineShop/GUI/productPageRelated/ProductPage.java
@@ -3,6 +3,7 @@ package ApProject_OnlineShop.GUI.productPageRelated;
 import ApProject_OnlineShop.GUI.FxmlController;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -15,6 +16,9 @@ import java.util.ResourceBundle;
 public class ProductPage extends FxmlController implements Initializable {
     public static long productId = 0;
     public ImageView image;
+    public Label name;
+    public Label brand;
+    public Label category;
 
     public void backButton(ActionEvent actionEvent) {
     }
