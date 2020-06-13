399fbd4563eae040aef20dc97f200f13d8289190
ASUS <yasamanzolfi4@gmail.com>
2020-06-14 00:24:42 +0430

    product page

--- src/main/java/ApProject_OnlineShop/GUI/productPageRelated/ProductPage.java
+++ src/main/java/ApProject_OnlineShop/GUI/productPageRelated/ProductPage.java
@@ -3,16 +3,17 @@ package ApProject_OnlineShop.GUI.productPageRelated;
 import ApProject_OnlineShop.GUI.FxmlController;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 import java.net.URL;
+import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 public class ProductPage extends FxmlController implements Initializable {
     public static long productId = 0;
-    public GridPane information;
     public ImageView image;
 
     public void backButton(ActionEvent actionEvent) {
@@ -24,7 +25,7 @@ public class ProductPage extends FxmlController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        image.setImage(new Image(Paths.get("Resources/productImages/"+ productId+".jpg").toUri().toString()));
     }
 
     public static void setProductId(long productId) {
