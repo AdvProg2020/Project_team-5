0c7ebac06bdbc56701fd4fd948c8f858e2f60ee7
ASUS <yasamanzolfi4@gmail.com>
2020-06-13 11:59:13 +0430

    work on product summery in all product page

--- src/main/java/ApProject_OnlineShop/GUI/ProductPageRelated/ProductBriefSummery.java
+++ src/main/java/ApProject_OnlineShop/GUI/ProductPageRelated/ProductBriefSummery.java
@@ -15,40 +15,56 @@ import javafx.scene.layout.VBox;
 import java.awt.*;
 
 public class ProductBriefSummery {
-    public GridPane getProductForAllProductsPage(){
-        GridPane gridPane = new GridPane();
-        setStyleForVBox(gridPane);
-        ImageView imageView = new ImageView(getClass().getClassLoader().getResource("pictures/shopLogo.png").toString());
-        imageView.setFitWidth(150);
+    public VBox getProductForAllProductsPage(){
+        VBox mainVBox = new VBox();
+        setStyleForVBox(mainVBox);
+        mainVBox.setAlignment(Pos.CENTER);
+        ImageView imageView = new ImageView(getClass().getClassLoader().getResource("pictures/7.jpg").toString());
+        VBox image = new VBox();
+        image.getChildren().add(imageView);
+        mainVBox.getChildren().add(image);
+        image.setMaxSize(150,150);
         imageView.setFitHeight(150);
-        gridPane.add(imageView,0,0,2,1);
-        GridPane.setHalignment(imageView, HPos.CENTER);
+        imageView.setFitWidth(150);
+        VBox nameVBox = new VBox();
+        nameVBox.setAlignment(Pos.CENTER_LEFT);
         Label name = new Label("Galaxy S9 Samsung");
         (new Styles()).setTextFont(name);
-        name.setPadding(new Insets(4,4,0,0));
-        gridPane.add(name,0,1);
-        GridPane.setHalignment(name,HPos.LEFT);
+        name.setPadding(new Insets(0,15,0,15));
+        nameVBox.getChildren().add(name);
+        mainVBox.getChildren().add(nameVBox);
+        HBox rateHBox = new HBox();
+        rateHBox.setAlignment(Pos.CENTER_LEFT);
         Label rate = new Label("8.0");
-        rate.setPadding(new Insets(4,0,0,4));
-        gridPane.add(rate,0,2 );
-        GridPane.setHalignment(rate,HPos.RIGHT);
+        rate.setPadding(new Insets(0,0,0,0));
+        rateHBox.setPadding(new Insets(0,15,0,8));
+        ImageView star = new ImageView(new Image(getClass().getClassLoader().getResource("pictures/star.png").toString()));
+        star.setFitWidth(20);
+        star.setFitHeight(20);
+        rateHBox.getChildren().add(star);
+        rateHBox.getChildren().add(rate);
+        mainVBox.getChildren().add(rateHBox);
         (new Styles()).setTextFont(rate);
+        rate.setPadding(new Insets(4,0,0,4));
+        VBox priceVBox = new VBox();
+        priceVBox.setAlignment(Pos.CENTER_LEFT);
+        priceVBox.setPadding(new Insets(0,15,0,15));
         Label price = new Label("9000" + " Rial");
-        price.setPadding(new Insets(4,4,0,0));
-        gridPane.add(price,0,3);
+        priceVBox.getChildren().add(price);
+        mainVBox.getChildren().add(priceVBox);
         (new Styles()).setTextFont(price);
-        GridPane.setHalignment(price,HPos.RIGHT);
-        return gridPane;
+        price.setPadding(new Insets(0,0,0,0));
+        return mainVBox;
     }
 
-    private void setStyleForVBox(GridPane gridPane){
+    private void setStyleForVBox(VBox gridPane){
         gridPane.setStyle("-fx-border-color:#8600b3;" +
                 "-fx-border-width: 1;" +
-                "-fx-border-style: solid;" +
-                "-fx-background-color: linear-gradient(to bottom right, #ffccff, #ffffff);");
+                "-fx-border-style: solid;"+
+                "-fx-background-color: #ffffff;");
         gridPane.setMaxSize(250,250);
         gridPane.setMinSize(250,250);
-        gridPane.setVgap(8);
+        gridPane.setSpacing(8);
     }
 
     public void setScene(){
