28ad70f497407524107351dce8d05f4528fdcbe5
ASUS <yasamanzolfi4@gmail.com>
2020-06-14 21:12:05 +0430

    view cart

--- src/main/java/ApProject_OnlineShop/GUI/productPageRelated/Cart.java
+++ src/main/java/ApProject_OnlineShop/GUI/productPageRelated/Cart.java
@@ -30,23 +30,24 @@ public class Cart extends FxmlController implements Initializable {
             HBox productBox = new HBox();
             setHBoxStyle(productBox);
             ImageView imageView = new ImageView(new Image(Paths.get("Resources/productImages/" + productId + ".jpg").toUri().toString()));
-            imageView.setFitWidth(199.5);
-            imageView.setFitHeight(199.5);
+            imageView.setFitWidth(149);
+            imageView.setFitHeight(149);
             productBox.getChildren().add(imageView);
             List<String> goodInfo = MainController.getInstance().getAccountAreaForCustomerController().viewGoodInCartById(productId);
             VBox textFieldVBox = new VBox();
+            textFieldVBox.setMinWidth(380);
             textFieldVBox.setMaxHeight(200);
             productBox.getChildren().add(textFieldVBox);
             HBox nameHBox = new HBox();
             nameHBox.setAlignment(Pos.CENTER_LEFT);
             Label name = new Label(goodInfo.get(0));
-            name.setPadding(new Insets(0, 15, 0, 15));
+            name.setPadding(new Insets(10, 15, 0, 15));
             name.setFont(Font.font("Times New Roman", 20));
             nameHBox.getChildren().add(name);
             textFieldVBox.getChildren().add(nameHBox);
             HBox sellerUserBox = new HBox();
             sellerUserBox.setAlignment(Pos.CENTER_LEFT);
-            Label seller = new Label(goodInfo.get(1));
+            Label seller = new Label("seller:  "+goodInfo.get(1));
             seller.setPadding(new Insets(10, 15, 10, 15));
             seller.setFont(Font.font("Times New Roman", 16));
             sellerUserBox.getChildren().add(seller);
@@ -56,27 +57,32 @@ public class Cart extends FxmlController implements Initializable {
             hBox.setMaxHeight(55);
             hBox.setMinHeight(55);
             hBox.setSpacing(15);
+            HBox space = new HBox();
+            space.setMinWidth(15);
+            hBox.getChildren().add(space);
             HBox numberBox = new HBox();
             numberBox.setMaxHeight(35);
             numberBox.setMinHeight(35);
-            numberBox.setMinWidth(100);
-            numberBox.setMaxWidth(100);
-            numberBox.setStyle("-fx-border-color:#8600b3;-fx-border-width: 1; -fx-border-style: solid;");
-            Text plus = new Text(" + ");
+            numberBox.setMinWidth(70);
+            numberBox.setMaxWidth(70);
+            numberBox.setStyle("-fx-border-color:#000000;-fx-border-width: 1; -fx-border-style: solid;");
+            Text plus = new Text("  +   ");
             plus.setFont(Font.font("Times New Roman", 18));
             plus.setStyle("-fx-font-weight: bold;");
             plus.setOnMouseClicked(e -> increaseProduct(productId));
             numberBox.getChildren().add(plus);
+            numberBox.setAlignment(Pos.CENTER);
             Text number = new Text(goodInfo.get(2));
             numberBox.getChildren().add(number);
             number.setFont(Font.font("Times New Roman", 16));
-            Text minus = new Text(" - ");
-            minus.setFont(Font.font("Times New Roman", 18));
+            Text minus = new Text("   - ");
+            minus.setFont(Font.font("Times New Roman", 22));
             minus.setStyle("-fx-font-weight: bold;");
             minus.setOnMouseClicked(e -> decreaseProduct(productId));
             numberBox.getChildren().add(minus);
             hBox.getChildren().add(numberBox);
             HBox priceBox = new HBox();
+            priceBox.setMinWidth(270);
             priceBox.setAlignment(Pos.CENTER_RIGHT);
             Label price = new Label(goodInfo.get(3) + " Rials");
             price.setFont(Font.font("Times New Roman", 16));
@@ -94,10 +100,10 @@ public class Cart extends FxmlController implements Initializable {
                 "-fx-border-width: 1;" +
                 "-fx-border-style: solid;" +
                 "-fx-background-color: linear-gradient(to bottom right, #ffb3ff, #ffffff);");
-        hBox.setMinHeight(200);
+        hBox.setMinHeight(150);
         hBox.setMinWidth(540);
         hBox.setMaxWidth(540);
-        hBox.setMaxHeight(200);
+        hBox.setMaxHeight(150);
     }
 
     public void decreaseProduct(long productId) {
