e35e186364112220c6110e7d0b098c0664fe4848
ASUS <yasamanzolfi4@gmail.com>
2020-06-14 20:22:41 +0430

    view cart

--- src/main/java/ApProject_OnlineShop/GUI/productPageRelated/Cart.java
+++ src/main/java/ApProject_OnlineShop/GUI/productPageRelated/Cart.java
@@ -3,12 +3,18 @@ package ApProject_OnlineShop.GUI.productPageRelated;
 import ApProject_OnlineShop.GUI.FxmlController;
 import ApProject_OnlineShop.controller.MainController;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Border;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
+import java.awt.*;
 import java.net.URL;
 import java.nio.file.Paths;
 import java.util.List;
@@ -30,7 +36,42 @@ public class Cart extends FxmlController implements Initializable {
             List<String> goodInfo = MainController.getInstance().getAccountAreaForCustomerController().viewGoodInCartById(productId);
             VBox textFieldVBox = new VBox();
             textFieldVBox.setMaxHeight(200);
+            productBox.getChildren().add(textFieldVBox);
             HBox nameHBox = new HBox();
+            nameHBox.setAlignment(Pos.CENTER_LEFT);
+            Label name = new Label(goodInfo.get(0));
+            name.setPadding(new Insets(0,15,0,15));
+            name.setFont(Font.font("Times New Roman", 20));
+            nameHBox.getChildren().add(name);
+            textFieldVBox.getChildren().add(nameHBox);
+            HBox sellerUserBox = new HBox();
+            sellerUserBox.setAlignment(Pos.CENTER_LEFT);
+            Label seller = new Label(goodInfo.get(1));
+            seller.setPadding(new Insets(10,15,10,15));
+            seller.setFont(Font.font("Times New Roman", 16));
+            sellerUserBox.getChildren().add(seller);
+            textFieldVBox.getChildren().add(sellerUserBox);
+            HBox hBox = new HBox();
+            hBox.setAlignment(Pos.CENTER_LEFT);
+            hBox.setMaxHeight(55);
+            hBox.setMinHeight(55);
+            hBox.setSpacing(15);
+            HBox numberBox = new HBox();
+            numberBox.setMaxHeight(35);
+            numberBox.setMinHeight(35);
+            numberBox.setStyle("-fx-border-color:#8600b3;-fx-border-width: 1; -fx-border-style: solid;");
+            Text plus = new Text(" +");
+            plus.setFont(Font.font("Times New Roman", 16));
+            plus.setStyle("-fx-font-weight: bold;");
+            //plus.setOnMouseClicked(e -> MainController.getInstance().getAccountAreaForCustomerController().increaseInCartProduct(productId));
+            numberBox.getChildren().add(plus);
+
+            Text minus = new Text(" +");
+            minus.setFont(Font.font("Times New Roman", 16));
+            minus.setStyle("-fx-font-weight: bold;");
+            minus.setOnMouseClicked(e -> MainController.getInstance().getAccountAreaForCustomerController().decreaseInCartProduct(productId));
+            numberBox.getChildren().add(minus);
+
             items.getChildren().add(productBox);
         }
     }
@@ -44,5 +85,13 @@ public class Cart extends FxmlController implements Initializable {
         hBox.setMinWidth(540);
         hBox.setMaxWidth(540);
         hBox.setMaxHeight(200);
+    }
+
+    public void decreaseProduct(long productId){
+
+    }
+
+    public void increaseProduct(long productId){
+        
     }
 }
