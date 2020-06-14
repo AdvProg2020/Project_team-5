af49fa18c56d8da885269901e236668a95d41f17
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-15 00:41:46 +0430

    manage categories add category

---
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/AddCategoryPageController.java
@@ -0,0 +1,14 @@
+package ApProject_OnlineShop.GUI.accountArea.accountAreaForManager;
+
+import ApProject_OnlineShop.GUI.FxmlController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class AddCategoryPageController extends FxmlController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
