a96f8f9e667a52496f6a0b94f384baf1b9ea0724
ASUS <yasamanzolfi4@gmail.com>
2020-06-14 18:36:33 +0430

    product page

--- src/main/java/ApProject_OnlineShop/GUI/productPageRelated/Cart.java
+++ src/main/java/ApProject_OnlineShop/GUI/productPageRelated/Cart.java
@@ -1,4 +1,29 @@
 package ApProject_OnlineShop.GUI.productPageRelated;
 
-public class Cart {
+import ApProject_OnlineShop.GUI.FxmlController;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Cart extends FxmlController implements Initializable {
+    public VBox items;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    public void setVBoxStyle(HBox hBox) {
+        hBox.setAlignment(Pos.TOP_LEFT);
+        hBox.setStyle("-fx-border-color:#8600b3;" +
+                "-fx-border-width: 1;" +
+                "-fx-border-style: solid;" +
+                "-fx-background-color: linear-gradient(to bottom right, #ffb3ff, #ffffff);");
+        hBox.setMinHeight(600);
+        hBox.setMinWidth(400);
+        hBox.setSpacing(10);
+    }
 }
