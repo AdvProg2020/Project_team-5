47486dddbb4282264933b13149e64e6afc449eb6
ASUS <yasamanzolfi4@gmail.com>
2020-06-14 21:46:07 +0430

    view cart

--- src/main/java/ApProject_OnlineShop/GUI/mainMenu/MainMenuController.java
+++ src/main/java/ApProject_OnlineShop/GUI/mainMenu/MainMenuController.java
@@ -44,4 +44,8 @@ public class MainMenuController extends FxmlController {
             Platform.exit();
         }
     }
+
+    public void cart() {
+        setScene("cart.fxml", "cart");
+    }
 }
