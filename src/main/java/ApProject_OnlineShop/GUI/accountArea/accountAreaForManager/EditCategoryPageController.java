aff479db291c8aecf56b2db281fef5be37f035e2
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-14 22:25:08 +0430

    manage categories edit category controller

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/EditCategoryPageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/EditCategoryPageController.java
@@ -2,15 +2,30 @@ package ApProject_OnlineShop.GUI.accountArea.accountAreaForManager;
 
 import ApProject_OnlineShop.GUI.FxmlController;
 import ApProject_OnlineShop.controller.MainController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class EditCategoryPageController extends FxmlController implements Initializable {
+    @FXML
+    private VBox allFieldsVBox;
+    @FXML
+    private Button editButton;
+    @FXML
+    private VBox singlePropertyVBox;
+    @FXML
+    private Label valueLabel;
+    @FXML
+    private TextField newValueField;
+    @FXML
+    private TextField newPropertyField;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -27,5 +42,14 @@ public class EditCategoryPageController extends FxmlController implements Initia
             MainController.getInstance().getLoginRegisterController().logoutUser();
             setScene("mainMenuLayout.fxml", "Main menu");
         }
+    }
+
+    public void onEditFieldPressed(ActionEvent actionEvent) {
+    }
+
+    public void onManageSubcatsPressed(ActionEvent actionEvent) {
+    }
+
+    public void onAddPropertyPressed(ActionEvent actionEvent) {
     }
 }
