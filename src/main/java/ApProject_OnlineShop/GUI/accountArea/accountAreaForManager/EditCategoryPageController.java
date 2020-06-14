bb4280f973aa87071c8d028994c378c2cae85d7e
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-14 21:46:50 +0430

    manage categories edit category back and logout

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/EditCategoryPageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForManager/EditCategoryPageController.java
@@ -1,14 +1,31 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForManager;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.controller.MainController;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class EditCategoryPageController extends FxmlController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void onBackButtonPressed() {
+        setScene("manageCategoriesPage.fxml", "account area");
+    }
+
+    public void onLogoutIconClicked() {
+        Optional<ButtonType> result = showAlert
+                (Alert.AlertType.CONFIRMATION, "Logout", "Logout", "are you sure to logout?");
+        if (result.get() == ButtonType.OK) {
+            MainController.getInstance().getLoginRegisterController().logoutUser();
+            setScene("mainMenuLayout.fxml", "Main menu");
+        }
     }
 }
