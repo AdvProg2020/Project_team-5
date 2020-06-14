e57fc08acdd698e70be1d146178dc055beb2370b
ASUS <yasamanzolfi4@gmail.com>
2020-06-14 19:18:25 +0430

    view cart

--- src/main/java/ApProject_OnlineShop/GUI/productPageRelated/Cart.java
+++ src/main/java/ApProject_OnlineShop/GUI/productPageRelated/Cart.java
@@ -1,13 +1,17 @@
 package ApProject_OnlineShop.GUI.productPageRelated;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.controller.MainController;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import javax.swing.text.html.ImageView;
 import java.net.URL;
+import java.nio.file.Paths;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class Cart extends FxmlController implements Initializable {
@@ -15,7 +19,20 @@ public class Cart extends FxmlController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        List<Long> productIds = MainController.getInstance().getAccountAreaForCustomerController().viewInCartProducts();
+        for (Long productId : productIds) {
+            HBox productBox = new HBox();
+            setHBoxStyle(productBox);
+            ImageView imageView = new ImageView(new Image(Paths.get("Resources/productImages/" + productId + ".jpg").toUri().toString()));
+            imageView.setFitWidth(199.5);
+            imageView.setFitHeight(199.5);
+            productBox.getChildren().add(imageView);
+            List<String> goodInfo = MainController.getInstance().getAccountAreaForCustomerController().viewGoodInCartById(productId);
+            VBox textFieldVBox = new VBox();
+            textFieldVBox.setMaxHeight(200);
+            HBox nameHBox = new HBox();
+            items.getChildren().add(productBox);
+        }
     }
     public void setHBoxStyle(HBox hBox) {
         hBox.setAlignment(Pos.CENTER_LEFT);
@@ -23,8 +40,9 @@ public class Cart extends FxmlController implements Initializable {
                 "-fx-border-width: 1;" +
                 "-fx-border-style: solid;" +
                 "-fx-background-color: linear-gradient(to bottom right, #ffb3ff, #ffffff);");
-        hBox.setMinHeight(540);
-        hBox.setMinWidth(200);
-        hBox.setSpacing(10);
+        hBox.setMinHeight(200);
+        hBox.setMinWidth(540);
+        hBox.setMaxWidth(540);
+        hBox.setMaxHeight(200);
     }
 }
