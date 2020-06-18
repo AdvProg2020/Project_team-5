ac20aa622779f933f2efa6536639b07c2081aeb8
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-19 02:39:15 +0430

    edit tests

--- src/test/java/modelTest/productThingsTest/DiscountCodeTest.java
+++ src/test/java/modelTest/productThingsTest/DiscountCodeTest.java
@@ -1,30 +1,28 @@
 package modelTest.productThingsTest;
 
 import ApProject_OnlineShop.database.Database;
+import ApProject_OnlineShop.model.Shop;
 import ApProject_OnlineShop.model.persons.Customer;
 import ApProject_OnlineShop.model.productThings.DiscountCode;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.Assert;
 
 import java.time.LocalDate;
 
 public class DiscountCodeTest {
-    private static Customer customer;
-    private static DiscountCode discountCode;
-
-    @Before
-    public void initializeRequiredValuesForUsingDiscountCodeTest() {
-        Database.getInstance().loadTestFolders();
-        customer = new Customer("admin", "sadegh", "majidi",
-                "sadegh@gmail.com", "09360000000", "1234", 12212);
-        discountCode = new DiscountCode("asdSDhjk43iu3tr",
-                LocalDate.now(), LocalDate.now(), 300L, 20);
-        discountCode.addCustomerToCode(customer,4);
-    }
 
     @Test
     public void decreaseNumberOfRemainedChancesAfterUsingDiscountCodeForValidCustomerTest() throws Exception {
+        Database.getInstance().loadTestFolders();
+        Customer customer = new Customer("admin", "sadegh", "majidi",
+                "sadegh@gmail.com", "09360000000", "1234", 12212);
+        DiscountCode discountCode = new DiscountCode("asdSDhjk43iu3tr",
+                LocalDate.parse("2020-06-09"), LocalDate.parse("2020-07-10"), 300L, 20);
+        Shop.getInstance().getAllDiscountCodes().add(discountCode);
+        Shop.getInstance().getAllPersons().add(customer);
+        discountCode.addCustomerToCode(customer,4);
         int expectedValue = 3;
         discountCode.discountBeUsedForCustomer(customer);
         int actualValue = discountCode.getIncludedCustomers().get(customer);
@@ -33,7 +31,7 @@ public class DiscountCodeTest {
 
     @Test
     public void RandomDiscountCodeStringLengthTest() {
-        int expectedLength = discountCode.getCode().length();
+        int expectedLength = 15;
         int actualLength = DiscountCode.generateRandomDiscountCode().length();
         Assert.assertEquals(expectedLength, actualLength);
     }
