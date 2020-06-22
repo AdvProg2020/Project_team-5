9fe70c53430ea73f7472852597bb58b8a6c7017f
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-22 16:11:55 +0430

    write new tests

--- src/test/java/controllerTest/moreTests.java
+++ src/test/java/controllerTest/moreTests.java
@@ -2,6 +2,7 @@ package controllerTest;
 
 import ApProject_OnlineShop.controller.MainController;
 import ApProject_OnlineShop.database.Database;
+import ApProject_OnlineShop.exception.productExceptions.ProductWithThisIdNotExist;
 import ApProject_OnlineShop.model.Shop;
 import ApProject_OnlineShop.model.category.Category;
 import ApProject_OnlineShop.model.category.SubCategory;
@@ -65,8 +66,75 @@ public class moreTests {
         Assert.assertEquals(output, MainController.getInstance().getAllProductsController().getOffProductBriefSummery(good.getGoodId()).get(0));
     }
 
+    @Test
+    public void getAllCategoriesListTest() {
+        Assert.assertEquals(1, MainController.getInstance().getAllProductsController().getAllCategories().size());
+    }
+
+    @Test
+    public void isInOffTest() {
+        Good good=new Good("phone", "samsung", Shop.getInstance().findSubCategoryByName("sub kabir"), "", new HashMap<>(), (Seller) Shop.getInstance().findUser("hi"), 9000L, 3);
+        Shop.getInstance().findSubCategoryByName("sub kabir").addGood(good);
+        Shop.getInstance().addGoodToAllGoods(good);
+        ArrayList<Good> offGoods = new ArrayList<>();
+        offGoods.add(good);
+        Off off = new Off(offGoods, LocalDate.parse("2020-06-20"), LocalDate.parse("2020-07-09"), 60000L, 30, (Seller) Shop.getInstance().findUser("hi"));
+        Shop.getInstance().addOff(off);
+        ((Seller)Shop.getInstance().findUser("hi")).addOff(off.getOffId());
+        Assert.assertTrue(MainController.getInstance().getAllProductsController().isInOff(good.getGoodId()));
+    }
+
+    @Test
+    public void getGoodsListTest() {
+        Good good=new Good("phone", "samsung", Shop.getInstance().findSubCategoryByName("sub kabir"), "", new HashMap<>(), (Seller) Shop.getInstance().findUser("hi"), 9000L, 3);
+        Shop.getInstance().findSubCategoryByName("sub kabir").addGood(good);
+        Shop.getInstance().addGoodToAllGoods(good);
+        Assert.assertEquals(1, MainController.getInstance().getAllProductsController().getGoods().size());
+    }
+
+    @Test
+    public void compareWithTest() {
+        Good good=new Good("phone", "samsung", Shop.getInstance().findSubCategoryByName("sub kabir"), "", new HashMap<>(), (Seller) Shop.getInstance().findUser("hi"), 9000L, 3);
+        Shop.getInstance().findSubCategoryByName("sub kabir").addGood(good);
+        Shop.getInstance().addGoodToAllGoods(good);
+        Good good2 =new Good("phone", "apple", Shop.getInstance().findSubCategoryByName("sub kabir"), "", new HashMap<>(), (Seller) Shop.getInstance().findUser("hi"), 9000L, 3);
+        Shop.getInstance().findSubCategoryByName("sub kabir").addGood(good2);
+        Shop.getInstance().addGoodToAllGoods(good2);
+        MainController.getInstance().getProductController().setGood(good);
+        try {
+            String output = "+---------------------------+---------------------------+---------------------------+\n" +
+                    "| property                  | good 1                    | good 2                    |\n" +
+                    "+---------------------------+---------------------------+---------------------------+\n" +
+                    "| name                      | phone                     | phone                     |\n" +
+                    "| brand                     | samsung                   | apple                     |\n" +
+                    "| average rate              | 0.0                       | 0.0                       |\n" +
+                    "| subCategory               | sub kabir                 | sub kabir                 |\n" +
+                    "| modification date         | 2020-06-22                | 2020-06-22                |\n" +
+                    "| seen number               | 0                         | 0                         |\n" +
+                    "| number of sellers         | 1                         | 1                         |\n" +
+                    "| minmum price of product   | 9000                      | 9000                      |\n" +
+                    "+---------------------------+---------------------------+---------------------------+\n";
+            Assert.assertNotEquals(output, MainController.getInstance().getProductController().compareWithAnotherProduct(good2.getGoodId()));
+        } catch (ProductWithThisIdNotExist productWithThisIdNotExist) {
+            Assert.assertTrue(true);
+        }
+    }
+
+    @Test
+    public void compareWithAnotherGoodGUITest() {
+        Good good=new Good("phone", "samsung", Shop.getInstance().findSubCategoryByName("sub kabir"), "", new HashMap<>(), (Seller) Shop.getInstance().findUser("hi"), 9000L, 3);
+        Shop.getInstance().findSubCategoryByName("sub kabir").addGood(good);
+        Shop.getInstance().addGoodToAllGoods(good);
+        Good good2 =new Good("phone", "apple", Shop.getInstance().findSubCategoryByName("sub kabir"), "", new HashMap<>(), (Seller) Shop.getInstance().findUser("hi"), 9000L, 3);
+        Shop.getInstance().findSubCategoryByName("sub kabir").addGood(good2);
+        Shop.getInstance().addGoodToAllGoods(good2);
+        MainController.getInstance().getProductController().setGood(good);
+        Assert.assertEquals(16, MainController.getInstance().getProductController().compareWithAnotherProductGUI(good2.getGoodId()).size());
+    }
+
+
     @After
     public void terminate() {
-
+        TestShop.clearShop();
     }
 }
