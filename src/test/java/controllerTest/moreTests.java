8c9f86c6b3e06db29cb30db15a82c067623d25a2
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-22 14:52:05 +0430

    write new tests

--- src/test/java/controllerTest/moreTests.java
+++ src/test/java/controllerTest/moreTests.java
@@ -1,5 +1,6 @@
 package controllerTest;
 
+import ApProject_OnlineShop.controller.MainController;
 import ApProject_OnlineShop.database.Database;
 import ApProject_OnlineShop.model.Shop;
 import ApProject_OnlineShop.model.category.Category;
@@ -7,12 +8,14 @@ import ApProject_OnlineShop.model.category.SubCategory;
 import ApProject_OnlineShop.model.persons.Company;
 import ApProject_OnlineShop.model.persons.Seller;
 import ApProject_OnlineShop.model.productThings.Good;
+import ApProject_OnlineShop.model.productThings.Off;
 import ApProject_OnlineShop.testThings.TestShop;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -34,15 +37,34 @@ public class moreTests {
         Shop.getInstance().addSubCategory(subCategory);
         Company company=new Company("salam","asfs","asdasd","addasd","999");
         Seller seller = new Seller("hi", "seller", "seller", "", "", "aa",company);
-        Good good=new Good("phone", "samsung", subCategory, "", new HashMap<>(), seller, 9000L, 3);
-        subCategory.addGood(good);
-        Shop.getInstance().addGoodToAllGoods(good);
+        Shop.getInstance().addPerson(seller);
     }
 
     @Test
     public void getBriefProductTest() {
         String output = "";
+        Good good=new Good("phone", "samsung", Shop.getInstance().findSubCategoryByName("sub kabir"), "", new HashMap<>(), (Seller) Shop.getInstance().findUser("hi"), 9000L, 3);
+        Shop.getInstance().findSubCategoryByName("sub kabir").addGood(good);
+        Shop.getInstance().addGoodToAllGoods(good);
+        output = output + good.getName() + " " + good.getBrand();
+        Assert.assertEquals(output, MainController.getInstance().getAllProductsController().getProductBrief(good.getGoodId()).get(0));
     }
+
+    @Test
+    public void getBriefProductOffTest() {
+        String output = "";
+        Good good=new Good("phone", "samsung", Shop.getInstance().findSubCategoryByName("sub kabir"), "", new HashMap<>(), (Seller) Shop.getInstance().findUser("hi"), 9000L, 3);
+        Shop.getInstance().findSubCategoryByName("sub kabir").addGood(good);
+        Shop.getInstance().addGoodToAllGoods(good);
+        ArrayList<Good> offGoods = new ArrayList<>();
+        offGoods.add(good);
+        Off off = new Off(offGoods, LocalDate.parse("2020-06-20"), LocalDate.parse("2020-07-09"), 60000L, 30, (Seller) Shop.getInstance().findUser("hi"));
+        Shop.getInstance().addOff(off);
+        ((Seller)Shop.getInstance().findUser("hi")).addOff(off.getOffId());
+        output = output + good.getName() + " " + good.getBrand();
+        Assert.assertEquals(output, MainController.getInstance().getAllProductsController().getOffProductBriefSummery(good.getGoodId()).get(0));
+    }
+
     @After
     public void terminate() {
 
