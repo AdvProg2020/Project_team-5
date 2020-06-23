8a75530c724ec7bc7f9aecccaf6e84ec33f38845
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-06-24 03:27:09 +0430

    work on database

--- src/main/java/ApProject_OnlineShop/database/DatabaseAuthentication.java
+++ src/main/java/ApProject_OnlineShop/database/DatabaseAuthentication.java
@@ -7,16 +7,14 @@ import java.sql.Statement;
 
 public class DatabaseAuthentication {
     public static void createConnection() {
-        Connection connection = null;
-        String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=AdventureWorks2017;user=sa;password=1051380";
+        Connection connection;
+        String connectionUrl = "jdbc:sqlserver://192.168.43.236\\MSSQLSERVER:1433;databaseName=AdventureWorks2017;user=sa;password=1051380";
         try {
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             connection = DriverManager.getConnection(connectionUrl);
             Statement stmt = connection.createStatement();
             String SQL = "SELECT * FROM Production.Product ORDER BY ProductID DESC";
             ResultSet rs = stmt.executeQuery(SQL);
-
-            // Iterate through the data in the result set and display it.
             while (rs.next()) {
                 System.out.println(rs.getString("ProductID") + " " + rs.getString("Name"));
             }
