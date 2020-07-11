744d7b9e1814dd7a785591cbdcce890a5d306a19
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-12 00:46:30 +0430

    annotation

--- src/main/java/ApProject_OnlineShop/database/SqlAPIs.java
+++ src/main/java/ApProject_OnlineShop/database/SqlAPIs.java
@@ -1,6 +1,13 @@
 package ApProject_OnlineShop.database;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class SqlAPIs<T> {
@@ -17,7 +24,19 @@ public abstract class SqlAPIs<T> {
     }
 
     public List<T> getAllObjects() {
-        return null;
+        EntityManager entityManager = EntityManagerProducer.getInstanceOfEntityManager();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<T> query = criteriaBuilder.createQuery(classOfT);
+        Root<T> root = query.from(classOfT);
+
+        query.select(root);
+        TypedQuery<T> typedQuery = entityManager.createQuery(query);
+        try {
+            return typedQuery.getResultList();
+        } catch (NoResultException e) {
+            System.out.println(e.getMessage());
+            return new ArrayList<>();
+        }
     }
 
     public List<T> getAllObjectsWithSort(String field, boolean asc) {
