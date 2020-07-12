91d47f11025187eaa09612b851209f5bdba8a9a2
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-12 14:56:02 +0430

    write sql api classes for each entity

---
+++ src/main/java/ApProject_OnlineShop/database/sqlMode/SqlGoodInCartApi.java
@@ -0,0 +1,9 @@
+package ApProject_OnlineShop.database.sqlMode;
+
+import ApProject_OnlineShop.model.productThings.GoodInCart;
+
+public class SqlGoodInCartApi extends SqlAPIs<GoodInCart> {
+    public SqlGoodInCartApi() {
+        super(GoodInCart.class);
+    }
+}
---
+++ src/main/java/ApProject_OnlineShop/database/sqlMode/SqlSellerRelatedInfoAboutGood.java
@@ -0,0 +1,9 @@
+package ApProject_OnlineShop.database.sqlMode;
+
+import ApProject_OnlineShop.model.productThings.SellerRelatedInfoAboutGood;
+
+public class SqlSellerRelatedInfoAboutGood extends SqlAPIs<SellerRelatedInfoAboutGood> {
+    public SqlSellerRelatedInfoAboutGood() {
+        super(SellerRelatedInfoAboutGood.class);
+    }
+}
