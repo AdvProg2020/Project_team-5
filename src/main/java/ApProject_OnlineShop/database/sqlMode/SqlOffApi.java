83579db7f8eace8b3beabc9e59aac3b789744887
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-12 14:06:54 +0430

    write sql api classes for each entity

---
+++ src/main/java/ApProject_OnlineShop/database/sqlMode/SqlOffApi.java
@@ -0,0 +1,9 @@
+package ApProject_OnlineShop.database.sqlMode;
+
+import ApProject_OnlineShop.model.productThings.Off;
+
+public class SqlOffApi extends SqlAPIs<Off> {
+    public SqlOffApi() {
+        super(Off.class);
+    }
+}
---
+++ src/main/java/ApProject_OnlineShop/database/sqlMode/SqlSellerApi.java
@@ -0,0 +1,9 @@
+package ApProject_OnlineShop.database.sqlMode;
+
+import ApProject_OnlineShop.model.persons.Seller;
+
+public class SqlSellerApi extends SqlAPIs<Seller> {
+    public SqlSellerApi() {
+        super(Seller.class);
+    }
+}
