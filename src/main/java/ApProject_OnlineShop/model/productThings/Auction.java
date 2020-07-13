dcbbd94081d8db9bcd3173de0b3683ce3cec2e29
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-13 13:43:29 +0430

    auction model

--- src/main/java/ApProject_OnlineShop/model/productThings/Auction.java
+++ src/main/java/ApProject_OnlineShop/model/productThings/Auction.java
@@ -1,5 +1,7 @@
 package ApProject_OnlineShop.model.productThings;
 
+import ApProject_OnlineShop.model.Shop;
+import ApProject_OnlineShop.model.persons.Customer;
 import ApProject_OnlineShop.model.persons.Seller;
 
 import java.time.LocalDate;
@@ -21,5 +23,57 @@ public class Auction {
         this.startDate = startDate;
         this.endDate = endDate;
         this.customersOfferedPrices = new HashMap<>();
+    }
+
+    public int getAuctionId() {
+        return auctionId;
+    }
+
+    public Good getGood() {
+        return Shop.getInstance().findGoodById(good);
+    }
+
+    public void setGood(Good good) {
+        this.good = good.getGoodId();
+    }
+
+    public Seller getSeller() {
+        return (Seller) Shop.getInstance().findUser(seller);
+    }
+
+    public void setSeller(Seller seller) {
+        this.seller = seller.getUsername();
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public void addOffer(Customer customer, long price) {
+        this.customersOfferedPrices.put(customer.getUsername(), price);
+    }
+
+    public void removeOffer(Customer customer) {
+        this.customersOfferedPrices.remove(customer.getUsername());
+    }
+
+    public HashMap<Customer, Long> getAllCustomersOffers() {
+        HashMap<Customer, Long> customerOffers = new HashMap<>();
+        for (String username : this.customersOfferedPrices.keySet()) {
+            customerOffers.put((Customer) Shop.getInstance().findUser(username), this.customersOfferedPrices.get(username));
+        }
+        return customerOffers;
     }
 }
