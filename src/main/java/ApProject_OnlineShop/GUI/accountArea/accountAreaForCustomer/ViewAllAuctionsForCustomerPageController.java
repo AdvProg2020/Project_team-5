0c5eaec3c328f63c6824d775bf30b9e41f88346d
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-14 14:19:07 +0430

    auction customer page

---
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForCustomer/ViewAllAuctionsForCustomerPageController.java
@@ -0,0 +1,14 @@
+package ApProject_OnlineShop.GUI.accountArea.accountAreaForCustomer;
+
+import ApProject_OnlineShop.GUI.FxmlController;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewAllAuctionsForCustomerPageController extends FxmlController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
