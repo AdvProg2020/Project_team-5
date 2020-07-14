6ea64a1a187baf87f1ba845702355b3c1101cfc8
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-14 14:27:30 +0430

    auction customer page

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForCustomer/ViewAllAuctionsForCustomerPageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForCustomer/ViewAllAuctionsForCustomerPageController.java
@@ -1,14 +1,41 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForCustomer;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.server.RequestForServer;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ViewAllAuctionsForCustomerPageController extends FxmlController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+    }
+
+    public void onBackButtonPressed(ActionEvent actionEvent) {
+        setScene("accountAreaForCustomer.fxml", "account area");
+    }
+
+    public void onLogoutIconClicked(MouseEvent mouseEvent) {
+        Optional<ButtonType> result = showAlert
+                (Alert.AlertType.CONFIRMATION, "Logout", "Logout", "are you sure to logout?");
+        if (result.get() == ButtonType.OK) {
+//            MainController.getInstance().getLoginRegisterController().logoutUser();
+//            Shop.getInstance().clearCart();
+            connectToServer(new RequestForServer("LoginRegisterController", "logoutUser", getToken(), null));
+            ArrayList<String> inputs = new ArrayList<>();
+            inputs.add(getId() + "");
+            connectToServer(new RequestForServer("AccountAreaForCustomerController", "clearCart", null, inputs));
+            FxmlController.setId(Long.parseLong(connectToServer(new RequestForServer("###cart", null, null, null))));
+            setToken(null);
+            setScene("mainMenuLayout.fxml", "Main menu");
+        }
     }
 }
