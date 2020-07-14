43457b2436cf52603de0b1cbcc8f6489560cd37a
ASUS <yasamanzolfi4@gmail.com>
2020-07-14 11:00:53 +0430

    chat page

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/ChatPageController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/ChatPageController.java
@@ -1,15 +1,19 @@
 package ApProject_OnlineShop.GUI.accountArea;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.model.Massage;
 import ApProject_OnlineShop.server.RequestForServer;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -17,8 +21,8 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class ChatPageController extends FxmlController implements Initializable {
-    private static String owner ;
-    private static String guest ;
+    private static String owner;
+    private static String guest;
     private static String path;
     private static String backTitle;
     public VBox vBox;
@@ -27,6 +31,29 @@ public class ChatPageController extends FxmlController implements Initializable 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         title.setText("chat with " + guest);
+        Massage massage = new Massage("kure","hale", "hi babe");
+        vBox.getChildren().add(getMassageVBox(massage));
+    }
+
+    public VBox getMassageVBox(Massage massage) {
+        VBox vBox = new VBox();
+        vBox.setStyle("-fx-border-color:#8600b3; -fx-border-width: 1;" +
+                "-fx-border-style: solid; -fx-background-color: linear-gradient(to bottom right, #ffb3ff, #ffffff);");
+        vBox.setAlignment(Pos.CENTER_LEFT);
+        Label senderName = new Label(massage.getSenderUserName());
+        senderName.setFont(Font.font("Times New Roman", 15));
+        senderName.setStyle("-fx-text-color: # #600080");
+        vBox.getChildren().add(senderName);
+        Label body = new Label(massage.getMassage());
+        VBox.setMargin(body,new Insets(6,0,0,0));
+        body.setFont(Font.font("Times New Roman", 13));
+        vBox.getChildren().add(body);
+        String time = "" + massage.getTime();
+        Label timeText = new Label(time.substring(0,2) + ":" + time.substring(3,5));
+        timeText.setFont(Font.font("Times New Roman", 13));
+        timeText.setPadding(new Insets(7,5,5,4));
+        vBox.getChildren().add(timeText);
+        return vBox;
     }
 
     public void backButton(ActionEvent actionEvent) {
