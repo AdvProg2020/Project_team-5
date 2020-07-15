ae506a6535fa4591af923d1e4180f57c8a8baa53
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-16 03:08:19 +0430

    auction customer page

--- src/main/java/ApProject_OnlineShop/controller/products/AuctionsController.java
+++ src/main/java/ApProject_OnlineShop/controller/products/AuctionsController.java
@@ -1,4 +1,18 @@
 package ApProject_OnlineShop.controller.products;
 
+import ApProject_OnlineShop.model.Massage;
+import ApProject_OnlineShop.model.Shop;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AuctionsController {
+    public List<Massage> getMassages(String auctionId) {
+        ArrayList<Massage> massages = new ArrayList<>();
+        for (Massage massage : Shop.getInstance().getMassages()) {
+            if (massage.getReceiverUserName().equals("auction_" + auctionId))
+                massages.add(massage);
+        }
+        return massages;
+    }
 }
