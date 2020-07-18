e1cf5f3df85818fc554fb33fdfb42a2a01d465f2
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-18 14:06:51 +0430

    file sale

--- src/main/java/ApProject_OnlineShop/server/Server.java
+++ src/main/java/ApProject_OnlineShop/server/Server.java
@@ -55,6 +55,25 @@ public class Server {
         }
     }
 
+    private static void fileTransferServerOnline() {
+        ServerSocket serverSocketForFile = null;
+        try {
+            serverSocketForFile = new ServerSocket(4444);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("server for transfer files is online...");
+        while (true) {
+            try {
+                Socket clientSocket = serverSocketForFile.accept();
+                DataOutputStream dataOutputStream = new DataOutputStream(new BufferedOutputStream(clientSocket.getOutputStream()));
+                DataInputStream dataInputStream = new DataInputStream(new BufferedInputStream(clientSocket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public static void addOnlineUser(Person person, String token) {
         onlineUsers.put(token, person);
     }
