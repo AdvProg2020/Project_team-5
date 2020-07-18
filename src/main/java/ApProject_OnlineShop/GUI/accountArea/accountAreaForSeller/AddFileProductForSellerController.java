509d93e39c3a578df3a5fa4513108fa9a04e10f5
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-18 20:19:04 +0430

    file sale

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/AddFileProductForSellerController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/AddFileProductForSellerController.java
@@ -1,6 +1,8 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForSeller;
 
+import ApProject_OnlineShop.GUI.ErrorPageFxController;
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.GUI.StageController;
 import ApProject_OnlineShop.model.RequestForServer;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,7 +12,9 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Optional;
@@ -25,6 +29,8 @@ public class AddFileProductForSellerController extends FxmlController implements
     private TextField description;
     @FXML
     private Button submitButton;
+
+    private File selectedFile = null;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -51,4 +57,36 @@ public class AddFileProductForSellerController extends FxmlController implements
         }
     }
 
+    public void onBrowseFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        selectedFile = fileChooser.showOpenDialog(StageController.getStage());
+        this.submitButton.setDisable(false);
+    }
+
+    public void onAddProduct(ActionEvent actionEvent) {
+        if (price.getText().isEmpty() || name.getText().isEmpty()) {
+            ErrorPageFxController.showPage("can not add good", "fields should be filled!");
+            return;
+        }
+        if (!price.getText().matches("\\d+")) {
+            ErrorPageFxController.showPage("can not add good", "price has incorrect format!");
+            this.name.clear();
+            this.price.clear();
+            this.description.clear();
+            return;
+        }
+        if (this.selectedFile == null) {
+            ErrorPageFxController.showPage("can not add good", "file of product should be added!");
+            this.name.clear();
+            this.price.clear();
+            this.description.clear();
+            return;
+        }
+        String name = this.name.getText();
+        String description = this.description.getText();
+        String price = this.price.getText();
+        if (description.isEmpty())
+            description = "empty!!";
+
+    }
 }
