7f25f9ae01591bc3747b2b2d08cfac240574216a
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-18 19:51:01 +0430

    file sale

--- src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/AddFileProductForSellerController.java
+++ src/main/java/ApProject_OnlineShop/GUI/accountArea/accountAreaForSeller/AddFileProductForSellerController.java
@@ -1,14 +1,54 @@
 package ApProject_OnlineShop.GUI.accountArea.accountAreaForSeller;
 
 import ApProject_OnlineShop.GUI.FxmlController;
+import ApProject_OnlineShop.model.RequestForServer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class AddFileProductForSellerController extends FxmlController implements Initializable {
+    @FXML
+    private TextField name;
+    @FXML
+    private TextField price;
+    @FXML
+    private TextField description;
+    @FXML
+    private Button submitButton;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
     }
+
+    public void onBackButtonPressed(ActionEvent actionEvent) {
+        setScene("accountAreaForSeller.fxml", "account area");
+    }
+
+    public void onLogoutIconClicked(MouseEvent mouseEvent) {
+        Optional<ButtonType> result = showAlert
+                (Alert.AlertType.CONFIRMATION, "Logout", "Logout", "are you sure to logout?");
+        if (result.get() == ButtonType.OK) {
+//            MainController.getInstance().getLoginRegisterController().logoutUser();
+//            Shop.getInstance().clearCart();
+            connectToServer(new RequestForServer("LoginRegisterController", "logoutUser", getToken(), getInputsForServer()));
+            ArrayList<String> inputs = new ArrayList<>();
+            inputs.add(getId() + "");
+            connectToServer(new RequestForServer("AccountAreaForCustomerController", "clearCart", null, inputs));
+            FxmlController.setId(Long.parseLong(connectToServer(new RequestForServer("###cart", null, null, null))));
+            setToken(null);
+            setScene("mainMenuLayout.fxml", "Main menu");
+        }
+    }
+
 }
