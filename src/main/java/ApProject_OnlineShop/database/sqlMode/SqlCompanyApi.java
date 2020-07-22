9e0394ee50b3b66df7b58f016f2fba4e48d0f454
sadegh-majidi80 <mohammadsadeghmajidiyazdi@hotmail.com>
2020-07-23 00:17:38 +0430

    database

---
+++ src/main/java/ApProject_OnlineShop/database/sqlMode/SqlCompanyApi.java
@@ -0,0 +1,9 @@
+package ApProject_OnlineShop.database.sqlMode;
+
+import ApProject_OnlineShop.model.persons.Company;
+
+public class SqlCompanyApi extends SqlAPIs<Company> {
+    public SqlCompanyApi() {
+        super(Company.class);
+    }
+}
